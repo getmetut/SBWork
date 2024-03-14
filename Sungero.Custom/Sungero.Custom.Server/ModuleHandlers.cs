7c6a9f3c759be7e53d132621568d94f2aa0c32a8
Igor Medvedev <iamedvedev@sberdevices.ru>
2024-03-14 12:04:45 +0300

    Импорт пакета разработки [Rx]

--- Sungero.Custom/Sungero.Custom.Server/ModuleHandlers.cs
+++ Sungero.Custom/Sungero.Custom.Server/ModuleHandlers.cs
@@ -6,7 +6,20 @@ using Sungero.CoreEntities;
 
 namespace Sungero.Custom.Server
 {
-  partial class FolderFolderHandlers
+  partial class AccountingDocumentsFolderHandlers
+  {
+
+    public virtual IQueryable<sberdev.SBContracts.IAccountingDocumentBase> AccountingDocumentsDataQuery(IQueryable<sberdev.SBContracts.IAccountingDocumentBase> query)
+    {
+      //query = query.Where(d => (d.AccessRights.IsGranted(Sungero.Core.DefaultAccessRightsTypes.Read, Users.Current)) ||
+      //                    (d.AccessRights.IsGranted(Sungero.Core.DefaultAccessRightsTypes.Change, Users.Current)) ||
+      //                    (d.AccessRights.IsGranted(Sungero.Core.DefaultAccessRightsTypes.FullAccess, Users.Current)));
+      query = query.Where(d => ((d.ProductListSDev.Length > 1) || (d.CalcListSDev.Length > 1)));
+      return query;
+    }
+  }
+
+  partial class ProdCalcFolderFolderHandlers
   {
 
     public virtual IQueryable<Sungero.Custom.IAuthorJob> FolderDataQuery(IQueryable<Sungero.Custom.IAuthorJob> query)
--- Sungero.Custom/Sungero.Custom.Server/PlusAssignment/PlusAssignmentHandlers.cs
+++ Sungero.Custom/Sungero.Custom.Server/PlusAssignment/PlusAssignmentHandlers.cs
@@ -7,4 +7,17 @@ using Sungero.Custom.PlusAssignment;
 
 namespace Sungero.Custom
 {
+  partial class PlusAssignmentServerHandlers
+  {
+
+    public override void Created(Sungero.Domain.CreatedEventArgs e)
+    {
+      var Task = Custom.NDATasks.Get(_obj.MainTask.Id);
+      if (Task.TravelDoc == Custom.NDATask.TravelDoc.Kurier)
+        _obj.TravelStr = Custom.PlusAssignment.TravelStr.Paper;
+      else
+        _obj.TravelStr = Custom.PlusAssignment.TravelStr.Electronic;
+    }
+  }
+
 }
