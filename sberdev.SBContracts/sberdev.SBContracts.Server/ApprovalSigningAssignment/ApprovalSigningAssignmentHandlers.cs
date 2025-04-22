311fd965d51499354fcc1b3ad56367e843a09adb
DirectumRXUser <tsasachev@sberdevices.ru>
2025-04-22 19:46:43 +0300

    Корректировки

--- sberdev.SBContracts/sberdev.SBContracts.Server/ApprovalSigningAssignment/ApprovalSigningAssignmentHandlers.cs
+++ sberdev.SBContracts/sberdev.SBContracts.Server/ApprovalSigningAssignment/ApprovalSigningAssignmentHandlers.cs
@@ -10,6 +10,17 @@ namespace sberdev.SBContracts
   partial class ApprovalSigningAssignmentServerHandlers
   {
 
+    public override void Saving(Sungero.Domain.SavingEventArgs e)
+    {
+      base.Saving(e);
+      var task = sberdev.SBContracts.ApprovalTasks.As(_obj.Task);
+      if (task != null)
+      {
+        _obj.AmountATSDevATSDev = task.AmountATSDev;
+        _obj.ContractTypeATSDevATSDev = task.ContractTypeATSDev;
+      }
+    }
+
     public override void BeforeComplete(Sungero.Workflow.Server.BeforeCompleteEventArgs e)
     {
       base.BeforeComplete(e);
