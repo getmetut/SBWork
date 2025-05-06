c57afa3b1811f25694afb6534ab99050e7655b0a
DirectumRXUser <tsasachev@sberdevices.ru>
2025-05-06 20:16:24 +0300

    [ebnf - jnrk.xtybt  [Rx]

---
+++ Sungero.ATS/Sungero.ATS.Server/DirRX.TeamsCommonAPI/ModuleJobs.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace Sungero.ATS.Module.TeamsCommonAPI.Server
+{
+  partial class ModuleJobs 
+  {
+
+  }
+}
