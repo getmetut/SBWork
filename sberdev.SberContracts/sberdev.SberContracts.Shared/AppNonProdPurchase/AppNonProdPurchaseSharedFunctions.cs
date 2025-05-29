90fa054d3b68dcd1587c22e3e4f5397bf5de1365
DirectumRXUser <tsasachev@sberdevices.ru>
2025-05-29 12:49:00 +0300

    + Непроизводственная закупка

---
+++ sberdev.SberContracts/sberdev.SberContracts.Shared/AppNonProdPurchase/AppNonProdPurchaseSharedFunctions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using sberdev.SberContracts.AppNonProdPurchase;
+
+namespace sberdev.SberContracts.Shared
+{
+  partial class AppNonProdPurchaseFunctions
+  {
+
+    /// <summary>
+    /// Обновление отображения полей карточки
+    /// </summary>       
+    public void UpdateCard()
+    {
+      
+    }
+
+  }
+}
